461e8ca688eda6c8ebfda4dded9da7790e9ea715
Krishan Wyse <kwysek@gmail.com>
2016-11-05 18:40:41 +0000

    Move App into separate module

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,12 @@
+//! Represents a YAML-based configuration file with optional overrides
+//! passed in via the command line.
+
+use clap::App;
+use serde_yaml;
 use std::error::Error;
 use std::io;
 use std::fmt;
 use std::path::Path;
-use clap::App;
-use serde_yaml;
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct Config {
@@ -20,6 +23,46 @@ impl Default for Config {
             frame_rate: 60.0,
         }
     }
+}
+
+pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<Config, ConfigError> {
+    use std::fs::File;
+
+    let config_file = try!(File::open(path));
+    let config = try!(serde_yaml::from_reader(config_file));
+
+    Ok(config)
+}
+
+pub fn apply_session_overrides(mut config: Config) -> Config {
+    let overrides = get_defined_cli().get_matches();
+    let overridden_value = |arg| overrides.value_of(arg).and_then(|val| val.parse::<u32>().ok());
+
+    if let Some(new_width) = overridden_value("width") { config.window_width = new_width }
+    if let Some(new_height) = overridden_value("height") { config.window_height = new_height }
+
+    config
+}
+
+fn get_defined_cli<'a, 'b>() -> App<'a, 'b> {
+    use clap::Arg;
+
+    App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(Arg::with_name("width")
+             .short("W")
+             .long("width")
+             .value_name("VALUE")
+             .help("Sets the width of the window")
+             .takes_value(true))
+        .arg(Arg::with_name("height")
+             .short("H")
+             .long("height")
+             .value_name("VALUE")
+             .help("Sets the height of the window")
+             .takes_value(true))
 }
 
 #[derive(Debug)]
@@ -63,44 +106,4 @@ impl Error for ConfigError {
             ConfigError::Parse(ref err) => Some(err)
         }
     }
-}
-
-pub fn load_from_file<P: AsRef<Path>>(path: P) -> Result<Config, ConfigError> {
-    use std::fs::File;
-
-    let config_file = try!(File::open(path));
-    let config = try!(serde_yaml::from_reader(config_file));
-
-    Ok(config)
-}
-
-pub fn apply_session_overrides(mut config: Config) -> Config {
-    let overrides = get_defined_cli().get_matches();
-    let overridden_value = |arg| overrides.value_of(arg).and_then(|val| val.parse::<u32>().ok());
-
-    if let Some(new_width) = overridden_value("width") { config.window_width = new_width }
-    if let Some(new_height) = overridden_value("height") { config.window_height = new_height }
-
-    config
-}
-
-fn get_defined_cli<'a, 'b>() -> App<'a, 'b> {
-    use clap::Arg;
-
-    App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(Arg::with_name("width")
-             .short("W")
-             .long("width")
-             .value_name("VALUE")
-             .help("Sets the width of the window")
-             .takes_value(true))
-        .arg(Arg::with_name("height")
-             .short("H")
-             .long("height")
-             .value_name("VALUE")
-             .help("Sets the height of the window")
-             .takes_value(true))
 }
