79441aeb1961c3ec5afb7959a912da8c5788f79c
Krishan Wyse <kwysek@gmail.com>
2016-11-06 13:44:15 +0000

    Clean up graphics module

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,32 +1,29 @@
 //! Abstractions for the OpenGL graphics pipeline
 
+use glium::{Frame, Program, Surface, VertexBuffer};
+use glium::backend::Facade;
+use glium::index::NoIndices;
+
+use config::Config;
+
+type Coord = (i32, i32);
+type Size = (i32, i32);
+
 #[derive(Debug, Clone, Copy)]
-struct Vertex {
+pub struct Vertex {
     position: [f32; 2],
 }
 
 implement_vertex!(Vertex, position);
 
-use glium::VertexBuffer;
-use glium::index::NoIndices;
-use glium::Program;
-use glium::Surface;
-
-pub struct Quad<'window> {
-    vs: [Vertex; 4],
-    display: &'window Facade,
+pub struct Quad {
     vertices: VertexBuffer<Vertex>,
     indices: NoIndices,
     program: Program,
 }
 
-use glium::backend::Facade;
-type Coord = (i32, i32);
-type Size = (i32, i32);
-use config::Config;
-
-impl<'window> Quad<'window> {
-    pub fn new<F: Facade>(display: &'window F, config: Config, origin: Coord, size: Size) -> Self {
+impl Quad {
+    pub fn new<'window, F: Facade>(display: &'window F, config: Config, origin: Coord, size: Size) -> Self {
         use glium::index::PrimitiveType;
 
         let p2u = pixel_to_unit;
@@ -40,39 +37,11 @@ impl<'window> Quad<'window> {
             Vertex { position: [p2u(origin.0 + size.0, width), p2u(height as i32 - origin.1 - size.1, height)] },
         ];
 
-        let vert = r#"
-            #version 140
-
-            in vec2 position;
-
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
-
-        let frag = r#"
-            #version 140
-
-            out vec4 color;
-
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
-
         Quad {
-            display: display,
             vertices: VertexBuffer::new(display, &vertices).unwrap(),
             indices: NoIndices(PrimitiveType::TriangleStrip),
-            program: Program::from_source(display, vert, frag, None).unwrap(),
-            vs: vertices,
+            program: Program::from_source(display, vertex_shader(), fragment_shader(), None).unwrap(),
         }
-    }
-
-    pub fn render<S: Surface>(&self, target: &mut S) {
-        use glium::uniforms::EmptyUniforms;
-
-        target.draw(&self.vertices, &self.indices, &self.program, &EmptyUniforms, &Default::default()).unwrap();
     }
 }
 
@@ -81,74 +50,61 @@ pub fn pixel_to_unit(pixel: i32, bound: u32) -> f32 {
     (pixel as f32 - origin) / origin
 }
 
-pub trait Render {
-    fn render<'window, 'entity, R: Renderable<'window, 'entity> + 'entity + 'window>(&mut self, renderable: &'entity R);
+fn vertex_shader() -> &'static str {
+    r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#
 }
 
-use glium::Frame;
+fn fragment_shader() -> &'static str {
+    r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#
+}
+
+pub trait Render {
+    fn render<'entity, R: Renderable<'entity> + 'entity>(&mut self, renderable: &'entity R);
+}
 
 impl Render for Frame {
-    fn render<'window, 'entity, R: Renderable<'window, 'entity> + 'entity + 'window>(&mut self, renderable: &'entity R) {
+    fn render<'entity, R: Renderable<'entity> + 'entity>(&mut self, renderable: &'entity R) {
         use glium::uniforms::EmptyUniforms;
-        use glium::index::PrimitiveType;
 
-        let vert = r#"
-            #version 140
+        let vertices = renderable.vertices();
+        let indices = renderable.indices();
+        let program = renderable.program();
 
-            in vec2 position;
-
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
-
-        let frag = r#"
-            #version 140
-
-            out vec4 color;
-
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
-
-        // let display = renderable.display();
-        // let vertices = VertexBuffer::new(display, renderable.vertices()).unwrap();
-        let indices = NoIndices(PrimitiveType::TriangleStrip);
-        let vertices = renderable.vertices2();
-        //let program = Program::from_source(display, vert, frag, None).unwrap();
-
-        self.draw(vertices, indices, renderable.program(), &EmptyUniforms, &Default::default()).unwrap();
+        self.draw(vertices, indices, program, &EmptyUniforms, &Default::default()).unwrap();
     }
 }
 
-use glium::Display;
-
-trait Renderable<'window, 'entity> {
-    fn vertices(&'entity self) -> &'entity [Vertex];
-    fn vertices2(&'entity self) -> &'entity VertexBuffer<Vertex>;
-    fn display(&'window self) -> &'window Facade;
+pub trait Renderable<'entity> {
+    fn vertices(&'entity self) -> &'entity VertexBuffer<Vertex>;
+    fn indices(&'entity self) -> &'entity NoIndices;
     fn program(&'entity self) -> &'entity Program;
 }
 
-impl<'window, 'entity> Renderable<'window, 'entity> for Quad<'window> {
-    fn vertices(&'entity self) -> &'entity [Vertex] {
-        &self.vs
-    }
-
-    fn vertices2(&'entity self) -> &'entity VertexBuffer<Vertex> {
+impl<'entity> Renderable<'entity> for Quad {
+    fn vertices(&'entity self) -> &'entity VertexBuffer<Vertex> {
         &self.vertices
     }
 
-    fn display(&'window self) -> &'window Facade {
-        self.display
+    fn indices(&'entity self) -> &'entity NoIndices {
+        &self.indices
     }
 
     fn program(&'entity self) -> &'entity Program {
         &self.program
     }
 }
-        
 
 #[cfg(test)]
 mod tests {
